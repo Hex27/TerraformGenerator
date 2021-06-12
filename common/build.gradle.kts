dd39f31668f18baa43cc53821f9f9ac0f9ac9c47
broccolai <me@broccol.ai>
2021-06-12 13:13:59 +0100

    Fix built Jar not working on Java 11
    Also contains other various build improvements.
    Maven local was temporarily added to the repo's until codemc fix their wrong nms publishing for 1.17.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,24 +1,6 @@
-plugins {
-    java
-}
-
 group = "org.terraform"
 
-repositories {
-    maven{ url = uri("https://repo.codemc.io/repository/nms/") }
-    mavenCentral()
-    flatDir {
-        dirs("../libs")
-    }
-}
-
 dependencies {
-    testImplementation("junit", "junit", "4.12")
     compileOnly(group = "org.spigotmc", name = "spigot", version = "1.16.4-R0.1-SNAPSHOT")
-    compileOnly(fileTree("../libs/"))
     compileOnly("org.jetbrains:annotations:20.1.0")
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
 }
