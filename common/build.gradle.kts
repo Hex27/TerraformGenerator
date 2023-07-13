0a05f90868ba25324e03fd55c80541cef26edcc3
mani123 <70480001+mani1232@users.noreply.github.com>
2023-07-13 15:08:32 +0200

    Fix

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,7 +2,7 @@ group = "org.terraform"
 
 dependencies {
     compileOnly(group = "org.spigotmc", name = "spigot", version = "1.18-R0.1-SNAPSHOT")
-    compileOnly("space.arim.morepaperlib:morepaperlib:0.4.2")
+    implementation("space.arim.morepaperlib:morepaperlib:0.4.2")
 	//compileOnly(group = "org.spigotmc", name = "spigot", version = "1.16.4-R0.1-SNAPSHOT")
     compileOnly("org.jetbrains:annotations:20.1.0")
 }
