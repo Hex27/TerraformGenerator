99560a34af54dd5668d87cf3efa4665d010001b3
Hex27 <leothepro555@gmail.com>
2024-08-18 19:32:03 +0800

    Fixed bad conflict deletion

--- common/src/main/java/org/terraform/structure/pillager/mansion/secondfloor/MansionSecondFloorBunkPopulator.java
+++ common/src/main/java/org/terraform/structure/pillager/mansion/secondfloor/MansionSecondFloorBunkPopulator.java
@@ -8,6 +8,7 @@ import org.bukkit.block.BlockFace;
 import org.jetbrains.annotations.NotNull;
 import org.terraform.coregen.populatordata.PopulatorDataAbstract;
 import org.terraform.data.SimpleBlock;
+import org.terraform.main.TerraformGeneratorPlugin;
 import org.terraform.schematic.TerraSchematic;
 import org.terraform.structure.pillager.mansion.MansionInternalWallState;
 import org.terraform.structure.pillager.mansion.MansionRoomPopulator;
