cca1daf99f87de2b40db3c1d52bd9a294ca008d3
Steven Van Ingelgem <steven@vaningelgem.be>
2024-09-04 14:29:57 +0200

    I saw bees didn't behave. Bees are animals. When animals are disabled, they still popped up. So bad bad bees! (#138)
    Fixed that.

--- common/src/main/java/org/terraform/utils/version/BeeHiveSpawner.java
+++ common/src/main/java/org/terraform/utils/version/BeeHiveSpawner.java
@@ -6,10 +6,13 @@ import org.jetbrains.annotations.NotNull;
 import org.terraform.coregen.populatordata.IPopulatorDataBeehiveEditor;
 import org.terraform.data.SimpleBlock;
 import org.terraform.main.TerraformGeneratorPlugin;
+import org.terraform.main.config.TConfig;
 
 public class BeeHiveSpawner {
 
     public static void spawnFullBeeNest(@NotNull SimpleBlock block) {
+        if (!TConfig.areAnimalsEnabled()) return;
+
         try {
             if (block.getPopData() instanceof IPopulatorDataBeehiveEditor ipopdata) {
                 ipopdata.setBeehiveWithBee(block.getX(), block.getY(), block.getZ());
