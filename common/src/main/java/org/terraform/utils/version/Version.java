1614525c046cd0ff4db004935a710b752eb9d0bf
Steven Van Ingelgem <steven@vaningelgem.be>
2024-12-01 22:33:11 +0100

    Fix init issue (#146)
    * Cleaner version injector
    
    * Bump yaml version

--- common/src/main/java/org/terraform/utils/version/Version.java
+++ common/src/main/java/org/terraform/utils/version/Version.java
@@ -6,15 +6,30 @@ import org.terraform.coregen.NMSInjectorAbstract;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Version {
 
     public static final String VERSION = Bukkit.getServer().getBukkitVersion().split("-")[0];
     public static final double DOUBLE = toVersionDouble(VERSION);
 
-    @Deprecated
-    public static boolean isAtLeast(@NotNull String version) {
-        return DOUBLE >= toVersionDouble(version);
+    private final static Map<Double, String> availableVersions = new HashMap<>();
+
+    static {
+        availableVersions.put(18.2, "v1_18_R2");
+        availableVersions.put(19.4, "v1_19_R3");
+        availableVersions.put(20.0, "v1_20_R1");
+        availableVersions.put(20.1, "v1_20_R1");
+        availableVersions.put(20.2, "v1_20_R2");
+        availableVersions.put(20.3, "v1_20_R3");
+        availableVersions.put(20.4, "v1_20_R3");
+        availableVersions.put(20.5, "v1_20_R4");
+        availableVersions.put(20.6, "v1_20_R4");
+        availableVersions.put(21.0, "v1_21_R1");
+        availableVersions.put(21.1, "v1_21_R1");
+        availableVersions.put(21.2, "v1_21_R2");
+        availableVersions.put(21.3, "v1_21_R2");
     }
 
     // Since I keep forgetting, an example version is 19.1 for 1.19.1
@@ -34,42 +49,19 @@ public class Version {
         return Double.parseDouble(version.substring(2));
     }
 
-    public enum SupportedVersion {
-        V_1_18_R2("v1_18_R2", 18.2),
-        V_1_19_R3("v1_19_R3", 19.4),
-        V_1_20_R1("v1_20_R1", 20, 20.1),
-        V_1_20_R2("v1_20_R2", 20.2),
-        V_1_20_R3("v1_20_R3", 20.3, 20.4),
-        V_1_20_R4("v1_20_R4", 20.5, 20.6),
-        V_1_21_R1("v1_21_R1", 21.0, 21.1),
-        V_1_21_R2("v1_21_R2", 21.2, 21.3),
-        ;
-        private final double[] versionDouble;
-        private final String packageName;
-
-        SupportedVersion(String packageName, double... versionDouble) {
-            this.versionDouble = versionDouble;
-            this.packageName = packageName;
-        }
-
-        public static @Nullable NMSInjectorAbstract getInjector() throws
-                ClassNotFoundException,
-                NoSuchMethodException,
-                InvocationTargetException,
-                InstantiationException,
-                IllegalAccessException
-        {
-            for (SupportedVersion sv : SupportedVersion.values()) {
-                for (double versionDouble : sv.versionDouble) {
-                    if (versionDouble != Version.DOUBLE) {
-                        continue;
-                    }
-                    return (NMSInjectorAbstract) Class.forName("org.terraform." + sv.packageName + ".NMSInjector")
-                                                      .getDeclaredConstructor()
-                                                      .newInstance();
-                }
-            }
+    public static @Nullable NMSInjectorAbstract getInjector() throws
+            ClassNotFoundException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            InstantiationException,
+            IllegalAccessException
+    {
+        if (!availableVersions.containsKey(Version.DOUBLE)) {
             return null;
         }
+
+        return (NMSInjectorAbstract) Class.forName("org.terraform." + availableVersions.get(Version.DOUBLE) + ".NMSInjector")
+                                                  .getDeclaredConstructor()
+                                                  .newInstance();
     }
 }
