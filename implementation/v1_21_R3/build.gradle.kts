243e61aee6466214347cb68d5bd8eee811592ae9
Hex27 <leothepro555@gmail.com>
2024-12-20 19:48:16 +0000

    Removed temporary measure to compile without codemc

--- implementation/v1_21_R3/build.gradle.kts
+++ implementation/v1_21_R3/build.gradle.kts
@@ -1,8 +1,6 @@
 dependencies {
     implementation(project(":common"))
-	//compileOnly(group = "org.spigotmc", name = "spigot", version = "1.21.4-R0.1-SNAPSHOT")
-	//Replace this local file stuff with the codemc repo above after they finally release 1.21.4.
-    compileOnly(files("../../libs/spigot-1.21.4-R0.1-SNAPSHOT.jar", "../../libs/spigot-api-1.21.4-R0.1-SNAPSHOT.jar","../../libs/datafixerupper-8.0.16.jar","../../libs/fastutil-8.5.15.jar","../../libs/brigadier-1.3.10.jar"))
+	compileOnly(group = "org.spigotmc", name = "spigot", version = "1.21.4-R0.1-SNAPSHOT")
 	compileOnly("org.jetbrains:annotations:20.1.0")
     compileOnly("com.github.AvarionMC:yaml:1.1.7")
 }
