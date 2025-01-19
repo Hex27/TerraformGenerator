438646777e0f64cb37787b94a3f128bb90eb2159
Hex27 <leothepro555@gmail.com>
2025-01-19 13:12:10 +0000

    Update to new paper repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
         mavenCentral()
         maven("https://repo.codemc.io/repository/nms/")
 		// maven("https://libraries.minecraft.net/minecraft-server")
-		maven("https://papermc.io/repo/repository/maven-public/")
+		maven("https://repo.papermc.io/repository/maven-public/")
 		maven("https://jitpack.io")
     }
 //    Handle this inside each implementation as different minecraft versions support a different max jvm version
