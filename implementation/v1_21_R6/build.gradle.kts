9ffea68a507eac0d8ea6fabad0393fbf6e870ebd
Hex27 <leothepro555@gmail.com>
2025-10-02 15:06:25 +0100

    Put codemc in

--- implementation/v1_21_R6/build.gradle.kts
+++ implementation/v1_21_R6/build.gradle.kts
@@ -1,8 +1,6 @@
 dependencies {
     implementation(project(":common"))
-	//compileOnly(group = "org.spigotmc", name = "spigot", version = "1.21.9-R0.1-SNAPSHOT")
-	//TODO: Replace with codemc when its out
-	compileOnly(fileTree("../../libs/"))
+	compileOnly(group = "org.spigotmc", name = "spigot", version = "1.21.9-R0.1-SNAPSHOT")
 	
 	compileOnly("org.jetbrains:annotations:20.1.0")
     compileOnly("com.github.AvarionMC:yaml:1.1.7")
