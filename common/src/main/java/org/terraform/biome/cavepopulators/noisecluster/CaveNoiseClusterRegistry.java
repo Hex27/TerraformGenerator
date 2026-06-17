f8a113d2a3412059f6febe029027da330cb83971
Hex27 <leothepro555@gmail.com>
2026-06-17 23:32:07 +0100

    Updated to 27.0.0 for real
    Ensured that sulfur caves do not spawn for <26.2

--- common/src/main/java/org/terraform/biome/cavepopulators/noisecluster/CaveNoiseClusterRegistry.java
+++ common/src/main/java/org/terraform/biome/cavepopulators/noisecluster/CaveNoiseClusterRegistry.java
@@ -1,14 +1,17 @@
 package org.terraform.biome.cavepopulators.noisecluster;
 
 import org.terraform.main.config.TConfig;
+import org.terraform.utils.version.Version;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class CaveNoiseClusterRegistry {
     public static Collection<AbstractNoiseClusterPopulator> initiateNoiseClusterPopulators(){
-        return List.of(
-                new SulfurNoiseClusterPopulator()
-        );
+        ArrayList<AbstractNoiseClusterPopulator> populators = new ArrayList<>();
+        if(Version.VERSION.isAtLeast(Version.v26_2))
+            populators.add(new SulfurNoiseClusterPopulator());
+        return populators;
     }
 }
